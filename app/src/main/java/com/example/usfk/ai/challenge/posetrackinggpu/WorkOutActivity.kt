a1c1df324ef40c97a2b5392b9b61303caeabd808
irack000 <IRACK000@gmail.com>
2023-01-29 03:06:01 +0900

    Feat: Squat Counting

--- app/src/main/java/com/example/usfk/ai/challenge/posetrackinggpu/WorkOutActivity.kt
+++ app/src/main/java/com/example/usfk/ai/challenge/posetrackinggpu/WorkOutActivity.kt
@@ -20,7 +20,6 @@ import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
-import androidx.camera.core.ImageAnalysis.Analyzer
 import androidx.core.view.WindowCompat
 import com.example.usfk.ai.challenge.R
 import com.google.mediapipe.apps.basic.BasicActivity
@@ -29,6 +28,7 @@ import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList
 import com.google.mediapipe.framework.Packet
 import com.google.mediapipe.framework.PacketGetter
 import com.google.protobuf.InvalidProtocolBufferException
+import kotlin.math.*
 
 /** Main activity of MediaPipe pose tracking app.  */
 class WorkOutActivity : BasicActivity() {
@@ -43,16 +43,26 @@ class WorkOutActivity : BasicActivity() {
     }
 
     private lateinit var poseTextView: TextView
+    private lateinit var startButton: Button
     private lateinit var poseAnalyzer: Pose
     private val recipeMap = ArmAnglesRecipe.toMap() + LegAnglesRecipe.toMap() + BodyAnglesRecipe.toMap()
     private val recipeList = recipeMap.toList().map { it.second }
     private val recipeName = recipeMap.toList().map { it.first }
+
+    private var isWorkOutStarted = false
+    private var count = 0
+    private var squatCriteria = listOf(77.33, 112.42, 115.12)  // knee, ankle, right hip to right knee
+    private var normalSituation = listOf(160.67, 150.15, 156.32)  // knee, ankle, right hip to right knee
+    private var threadsHold = 98.9
+    private var hitCriteria = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setStatusBarTransparent()
         super.onCreate(savedInstanceState)
         poseTextView = findViewById(R.id.pose_text)
         findViewById<Button>(R.id.flip_button)?.setOnClickListener { flipCamera() }
+        startButton = findViewById(R.id.squat_button)
+        startButton.setOnClickListener { startSquat() }
 
         // To show verbose logging, run:
         // adb shell setprop log.tag.MainActivity VERBOSE
@@ -68,9 +78,32 @@ class WorkOutActivity : BasicActivity() {
                     poseAnalyzer.setData(poseLandmarks)
                 }
                 val anal = poseAnalyzer.getAngleList(recipeList)
-                poseTextView.text = anal.mapIndexed { index, value ->
-                    "${recipeName[index]}:  ${"%.2f".format(value)}${if (index % 2 == 0 && index < 8) "   |   " else "\n"}"
-                }.joinToString("")
+                if (isWorkOutStarted) {
+                    // Euclidean Distance (MAX Dis 623.54)
+                    val currentRight = listOf(anal[4], anal[6], anal[8])
+                    val currentLeft = listOf(anal[5], anal[7], anal[9])
+                    var rightPercent = 0.0
+                    var leftPercent = 0.0
+                    if (!hitCriteria) {
+                        rightPercent = getPercent(getEuclideanDistance(squatCriteria, currentRight))
+                        leftPercent = getPercent(getEuclideanDistance(squatCriteria, currentLeft))
+                        if (rightPercent >= threadsHold && leftPercent >= threadsHold) {
+                            hitCriteria = true
+                        }
+                    } else {
+                        rightPercent = getPercent(getEuclideanDistance(normalSituation, currentRight))
+                        leftPercent = getPercent(getEuclideanDistance(normalSituation, currentLeft))
+                        if (rightPercent >= threadsHold && leftPercent >= threadsHold) {
+                            hitCriteria = false
+                            count++
+                        }
+                    }
+                    poseTextView.text = "<Next Posture> ${if (!hitCriteria) "Squat Down" else "Squat Up"}\n(R)Posture Correspondance: ${"%.2f".format(rightPercent)}\n(L)Posture Correspondance: ${"%.2f".format(leftPercent)}\nSquat Count: $count"
+                } else {
+                    poseTextView.text = anal.mapIndexed { index, value ->
+                        "${recipeName[index]}:  ${"%.2f".format(value)}${if (index % 2 == 0 && index < 8) "  |  " else "\n"}"
+                    }.joinToString("")
+                }
                 //poseTextView.text = logs.replace("\n\n", " ")
                 //logs = logs.replace("\n\n", "\n")
                 //if (Log.isLoggable(TAG, Log.VERBOSE)) Log.v(TAG, "[TS:" + packet.timestamp + "] " + logs)
@@ -79,6 +112,11 @@ class WorkOutActivity : BasicActivity() {
             }
         }
     }
+
+    private fun getEuclideanDistance(criteria: List<Double>, current: List<Double>) =
+        sqrt((criteria[0]-current[0]).pow(2) + (criteria[1]-current[1]).pow(2) + (criteria[2]-current[2]).pow(2))
+
+    private fun getPercent(distance: Double): Double = (623.54 - distance) / 6.2354
 
     private fun getPoseLandmarksDebugString(poseLandmarks: NormalizedLandmarkList): String {
         var poseLandmarkStr = """
@@ -102,6 +140,16 @@ class WorkOutActivity : BasicActivity() {
     fun flipCamera() {
         if (PermissionHelper.cameraPermissionsGranted(this)) {
             startCamera(!isCurrentCameraFacingFront)
+        }
+    }
+
+    fun startSquat() {
+        if (isWorkOutStarted) {
+            isWorkOutStarted = false
+            startButton.text = "Start Squat"
+        } else {
+            isWorkOutStarted = true
+            startButton.text = "Stop Squat"
         }
     }
 
